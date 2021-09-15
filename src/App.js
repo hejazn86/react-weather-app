881b2002da8ff3f85a8805fe34021ac47e50efc8
Hejaz Nawasser <hejazn@gmail.com>
2021-09-15 22:19:32 +0430

    adding the link of the source code

--- src/App.js
+++ src/App.js
@@ -6,11 +6,26 @@ import DailyWeather from './DailyWeather';
 function App() {
   return (
     <div className="App">
+      <div className='App-container'>
 
         <WeatherSearch className='Weather-Search'/>
         <DailyWeather className='Daily-Weather' />
-        <Forecast className='Forecast'/>
- 
+        <Forecast className='Forecast'/> 
+    </div>
+    <div>
+      <small clas="source-link">
+        <a
+        href="https://github.com/hejazn86/react-weather-app"
+        target="_blank"
+        class="text-decoration-none"
+        >
+          open-source code,{" "} 
+          </a>
+          <span>
+           by Hejaz Nawasser
+          </span>
+          </small>
+    </div>
     </div>
   );
 }
