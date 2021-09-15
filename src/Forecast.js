53d3e42f742be7f7af55f5a1dadf01f87a6bb443
Hejaz Nawasser <hejazn@gmail.com>
2021-09-15 15:25:42 +0430

    fixing some CSS styling of the components

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './WeeklyWeather.css';
+import './Forecast.css';
 import ReactAnimatedWeather from 'react-animated-weather/build/ReactAnimatedWeather';
 
 export default function Forecast(){
